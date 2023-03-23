5f5da69113b7afd18e35577290107d028f706fc1
Kevin Bollman <digitalkdogg@gmail.com>
2023-03-22 19:26:19 -0500

    test api poc

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::group([
+//    'namespace' => 'Customers', //namespace App\Http\Controllers\Customers;
+//    'middleware' => 'auth:api', // this is for check user is logged in or authenticated user
+//    'prefix' => 'customers' // you can use custom prefix for your rote {{host}}/api/customers/
+
+//], function ($router) {
+    // add and delete customer groups
+//    Route::get('/', [CustomerController::class, 'index']); // {{host}}/api/customers/  this is called to index method in CustomerController.php
+//    Route::post('/create', [CustomerController::class, 'create']); // {{host}}/api/customers/create this is called to create method in CustomerController.php
+//    Route::post('/show/{id}', [CustomerController::class, 'show']); // {{host}}/api/customers/show/10 this is called to show method in CustomerController.php parsing id to get single data
+//    Route::post('/delete/{id}', [CustomerController::class, 'delete']); // {{host}}/api/customers/delete/10 this is called to delete method in CustomerController.php for delete single data
+//});
+
+Route::get('/test', function () {
+    $data = ['message' => 'No new orders!','kevin'=>'bollman'];
+
+    return response($data, 200)->header('Content-Type', 'application/json');
+    //return response('Test API', 200)
+    //   ->header('Content-Type', 'application/json');
+});
