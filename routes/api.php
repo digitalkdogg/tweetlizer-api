06b8ba8a1f3d638a83ff8778d14ae6eb7b925d72
Kevin Bollman <digitalkdogg@gmail.com>
2023-03-24 10:44:38 -0500

    add ip to not auth

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RandomGeneratorController;
 use Request;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
